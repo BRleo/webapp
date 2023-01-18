3ac13ff5adeb40dd3e57c4ea28cd1d6313d651bb
BRleo <122840244+BRleo@users.noreply.github.com>
2023-01-17 23:13:51 -0300

    commit 2
    atualizando os comandos de js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-alert(!"test")
+//alert("test")
+
+//var nome =  prompt("leonardo");
+
+// function entrar(){
+//     var nome = prompt("Digite seu nome");
+//     document.write("Bem vindo" + " " + nome)
+// }
+
+// document.write("Bem vindo" +" "+ nome);
+
+// var lista = [5,6,7,8]
+
+// var sel = prompt("digite um numero");
+
+// document.write(lista[sel]);
+
+let lista = {
+nome: "Leonardo",
+idade: "27"
+};
+
+document.write(lista.nome);  
